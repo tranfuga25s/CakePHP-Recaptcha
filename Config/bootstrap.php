c83738f9e3dc3a281a80907e5df7692d28a171a6
tranfuga25s <tranfuga25s@gmail.com>
2014-09-20 20:38:31 -0300

    Agregado codigo anterior

---
+++ Config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+Configure::write('Recaptcha.publicKey', '6Lds-N4SAAAAANf1KEXnVnK0OurNZW2GIDztZl-k');
+Configure::write('Recaptcha.privateKey', '6Lds-N4SAAAAAF9YWyk9EdI_LeEMXDTfBfne5eE8');
+?>
